5e99691f73fb31b88b8f103345fa044c92ee41c4
Brian Lee <briandl92391@gmail.com>
2025-02-10 10:54:47 -0800

    feat(zsh): zsh shell configuration updates

---
+++ config/zsh/zprofile.zsh
@@ -0,0 +1,28 @@
+# zsh customizations
+[[ ! -f "$HOME/.config/zsh/common-aliases.zsh" ]] || source "$HOME/.config/zsh/common-aliases.zsh"
+
+# brew
+[[ -f /opt/homebrew/bin/brew ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# direnv
+command -v direnv >/dev/null 2>&1 && eval "$(direnv hook zsh)"
+
+# asdf
+if [[ -d "$HOME/.asdf" ]]; then
+  source "$HOME/.asdf/asdf.sh"
+  fpath=(${ASDF_DIR}/completions $fpath)
+  autoload -Uz compinit && compinit
+
+  # asdf plugins
+  [[ -d "$HOME/.asdf/plugins/golang" ]] && source ~/.asdf/plugins/golang/set-env.zsh
+  [[ -d "$HOME/.asdf/plugins/java" ]] && source ~/.asdf/plugins/java/set-java-home.zsh
+fi
+
+export LSCOLORS=exfxcxdxbxegedabagacad
+export EDITOR=nvim
+export PATH="$HOME/.local/bin:$PATH"
+
+# when using tmux reverse search is not working
+# I'm not sure why I need this, but it corrects the issue
+bindkey '^R' history-incremental-search-backward
+
--- config/zsh/zshrc.zsh
+++ config/zsh/zshrc.zsh
@@ -1,8 +1,3 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-if [[ -d "$HOME/powerlevel10k" ]]; then
-  source "$HOME/powerlevel10k/powerlevel10k.zsh-theme"
-fi
-
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
@@ -31,32 +26,3 @@ zinit snippet OMZL::directories.zsh
 if [[ -f "$HOME/.p10k.zsh" ]]; then
   source "$HOME/.p10k.zsh"
 fi
-
-# add some color to directory listing
-export LSCOLORS=exfxcxdxbxegedabagacad
-
-# zsh customizations
-[[ ! -f "$HOME/.config/zsh/common-aliases.zsh" ]] || source "$HOME/.config/zsh/common-aliases.zsh"
-
-# brew
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# direnv
-eval "$(direnv hook zsh)"
-
-# asdf
-source "$HOME/.asdf/asdf.sh"
-fpath=(${ASDF_DIR}/completions $fpath)
-autoload -Uz compinit && compinit
-
-# asdf plugins
-source ~/.asdf/plugins/golang/set-env.zsh
-source ~/.asdf/plugins/java/set-java-home.zsh
-
-export EDITOR=nvim
-export PATH="$HOME/.local/bin:$PATH"
-
-# when using tmux reverse search is not working
-# I'm not sure why I need this, but it corrects the issue
-bindkey '^R' history-incremental-search-backward
-
